e6e7b9a21af48c4c3b8a4e9c8ff802e890af78ed
uf-code <ugur.firat.developer@gmail.com>
2021-10-08 20:22:50 +0300

    Adding some explanation lines to functions

--- 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.py
+++ 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.py
@@ -4,14 +4,18 @@ class Vigenere_Cipher_With_ASCII:
         self.mod = 140
         self.last_layer = 194
 
+    # Converting every single characther into their ascii code
     def ascii_layer(self):
         numerical_list = [ord(c) for c in self.message]
         return numerical_list
 
+    # Adding a numerical key into each ascii lists item
     def key_layer(self, key, numerical_list):
         key_added_list = [n + key for n in numerical_list]
         return key_added_list
     
+    # Extending a keyword list to match lenght of the message
+    # Converting every single characther into their ascii code
     def keyword_layer(self, keyword):
         formatted_keyword_list = list(keyword.replace(" ", ""))
         extended_keyword_list = []
@@ -28,6 +32,9 @@ class Vigenere_Cipher_With_ASCII:
         extended_ascii_keyword_list = [ord(c) for c in extended_keyword_list]
         return extended_ascii_keyword_list
     
+    # Adding key added message and extended keyword list together
+    # Taking a mod of that combined list
+    # Converting every single ascii code into their Charachter Value
     def mod_layer(self, key_added_list, extended_keyword_list):
         modded_list = [ (key_added_list[i] + extended_keyword_list[i]) % self.mod for i in range(len(key_added_list)) ]
         result_text = [ chr( n + self.last_layer ) for n in modded_list ]
@@ -44,7 +51,9 @@ class Vigenere_Cipher_With_ASCII:
 
 
     def remove_mode_layer(self, encrypted_text):
-        remove_last_layer = [ ord(c) - self.last_layer for c in list(encrypted_text) ]
+        remove_last_layer = [ (ord(c) - self.last_layer) + self.mod for c in list(encrypted_text) ]
+        
+        
 
 
 
