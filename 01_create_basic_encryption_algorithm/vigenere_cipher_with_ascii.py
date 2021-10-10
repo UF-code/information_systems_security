05014fde57b2b2a1aa1e2bcb245c08834dfa2f43
uf-code <ugur.firat.developer@gmail.com>
2021-10-10 12:35:50 +0300

    Changing some explanation

--- 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.py
+++ 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.py
@@ -87,5 +87,5 @@ class Vigenere_Cipher_With_ASCII:
         
 
 if __name__ == "__main__":
-    # Encryption default & if you wanna chose decryption you have to specify system arg as 'dec'
+    # if system argument is "enc" Encryption & if you wanna chose decryption you have to specify system arg as 'dec'
     Vigenere_Cipher_With_ASCII().decryption() if sys.argv[1] == 'dec' else Vigenere_Cipher_With_ASCII().encryption() if sys.argv[1] == "enc" else print("Invalid Credentials")
