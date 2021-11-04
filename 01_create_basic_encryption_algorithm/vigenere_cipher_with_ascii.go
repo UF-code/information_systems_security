ebc7786e2c8dc83a1b813436174f657d2cea0e71
uf-code <ugur.firat.developer@gmail.com>
2021-11-04 09:35:45 +0300

    adding keyword as ascii

--- 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.go
+++ 01_create_basic_encryption_algorithm/vigenere_cipher_with_ascii.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -9,7 +12,6 @@ func convert_to_ascii(plain_text string) ([]int, int) {
 	// ascii := []int{}
 	var ascii []int
 	for _, x := range plain_text {
-		// fmt.Print(int(x), "\n")
 		ascii = append(ascii, int(x))
 	}
 
@@ -23,7 +25,7 @@ func add_key(ascii []int, key int) []int {
 	return ascii
 }
 
-func generate_keyword_list_add(length_ascii int, keyword string) []int {
+func generate_keyword_list(length_ascii int, keyword string) []int {
 	var extended_keyword_list []string
 	counter := 0
 	for {
@@ -42,8 +44,54 @@ func generate_keyword_list_add(length_ascii int, keyword string) []int {
 	return ascii
 }
 
-func convert_to_char(ascii_text []int) ([]string, int) {
+func add_keyword(key_added_ascii []int, keyword_list []int) []int {
+	for i := range key_added_ascii {
+		key_added_ascii[i] += keyword_list[i]
+	}
+	return key_added_ascii
+}
 
+// ENCRYPTION
+func encryption(mod int) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Message about to be encrypted: ")
+	scanner.Scan()
+	message := scanner.Text()
+	// fmt.Printf("%q", message)
+	// convert to ascii
+	result_ascii, length_ascii := convert_to_ascii(message)
+	// fmt.Print(result_ascii, length_ascii)
+
+	// adding key
+	fmt.Print("Key: ")
+	scanner.Scan()
+	key, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	key_added_ascii := add_key(result_ascii, int(key))
+
+	fmt.Print(key_added_ascii)
+	fmt.Print("\n")
+
+	fmt.Print("Keyword: ")
+	scanner.Scan()
+	keyword := scanner.Text()
+	keyword_list := generate_keyword_list(length_ascii, keyword)
+	fmt.Print(keyword_list)
+
+	// fmt.Print("\n")
+
+	// // test := add_key(result_ascii, 100)
+	// // fmt.Print(test)
+	// test := generate_keyword_list(length_ascii, "test")
+	// fmt.Print(test)
+
+	// fmt.Print("\n")
+
+	// result_char, length_char := convert_to_char(result_ascii)
+	// fmt.Print(result_char, length_char)
+}
+
+// DECRYPTION
+func convert_to_char(ascii_text []int) ([]string, int) {
 	var char []string
 	for _, x := range ascii_text {
 		// fmt.Print(string(x), "\n")
@@ -54,22 +102,11 @@ func convert_to_char(ascii_text []int) ([]string, int) {
 
 }
 
+func decryption() {
+
+}
+
 func main() {
 	mod := 256
-
-	fmt.Println(mod)
-	result_ascii, length_ascii := convert_to_ascii("ugur firat")
-	fmt.Print(result_ascii, length_ascii)
-
-	fmt.Print("\n")
-
-	// test := add_key(result_ascii, 100)
-	// fmt.Print(test)
-	test := generate_keyword_list_add(length_ascii, "test")
-	fmt.Print(test)
-
-	fmt.Print("\n")
-
-	result_char, length_char := convert_to_char(result_ascii)
-	fmt.Print(result_char, length_char)
+	encryption(mod)
 }
